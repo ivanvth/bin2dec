4edd41b8d9cad2067d19b0092170187897104271
Ivan Thrysøe <ivanvt@gmail.com>
2022-02-17 00:23:22 +0100

    omni done

--- Bin2dec.cs
+++ Bin2dec.cs
@@ -4,10 +4,11 @@ public static class Bin2dec {
         int sum = 0;
         for (int i = 0; i < bin.Length; i++) {
             switch (bin[i]) {
-                    case '0':   break;
-                    case '1':   sum += ConvertN(bin.Length - i - 1);
-                                break;
-                    default:    throw new ArgumentOutOfRangeException("That is not a valid binary number.");
+                case '0': break;
+                case '1':
+                    sum += ConvertN(bin.Length - i - 1);
+                    break;
+                default: throw new ArgumentOutOfRangeException("That is not a valid binary number.");
             }
         }
         return sum;
